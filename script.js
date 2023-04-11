602d0799a6a062e1bb2e85e029a67827a776e865
Sophie McNally <soph_mcnally@msn.com>
2023-04-11 16:26:41 +0100

    adding url for current weather data

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
  var apiKey = 'e453b657439ab759f676b00e45336611';
- 
+
+ function Currlocation(city){
+
+ var URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+ console.log(city)
+ }
