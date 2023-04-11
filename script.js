bf433e8c601c94d3fd11234fecfcf8902617b2fc
Sophie McNally <soph_mcnally@msn.com>
2023-04-11 16:37:29 +0100

    creating some query selectors to connect js to html

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
  var apiKey = 'e453b657439ab759f676b00e45336611';
 
+ var searchForm = document.querySelector('#search-form');
+ var searchInput = document.querySelector('#search-input');
+ var todayWeather = document.querySelector('#today');
+ var forecastWeather = document.querySelector('#forecast');
+
  function Currlocation(city){
 
  var URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
